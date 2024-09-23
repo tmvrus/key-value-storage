714e2ed103842fb05361c10ee9c84d9430bc8805
Mikhail Tsimbalov <tmvrus83@gmail.com>
2024-09-23 22:42:49 +0300

    protect in-memory with mutex

--- internal/storage/engine/inmemory/engine.go
+++ internal/storage/engine/inmemory/engine.go
@@ -2,11 +2,13 @@ package inmemory
 
 import (
 	"context"
+	"sync"
 
 	"github.com/tmvrus/key-value-storage/internal/domain"
 )
 
 type engine struct {
+	lock sync.RWMutex
 	data map[string]string
 }
 
@@ -15,11 +17,17 @@ func New() *engine {
 }
 
 func (e *engine) Set(_ context.Context, key, value string) error {
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
 	e.data[key] = value
 	return nil
 }
 
 func (e *engine) Get(_ context.Context, key string) (string, error) {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+
 	v, ok := e.data[key]
 	if !ok {
 		return "", domain.ErrNotFound
@@ -29,6 +37,9 @@ func (e *engine) Get(_ context.Context, key string) (string, error) {
 }
 
 func (e *engine) Delete(_ context.Context, key string) error {
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
 	_, ok := e.data[key]
 	if !ok {
 		return domain.ErrNotFound
