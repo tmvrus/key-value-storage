5a172a07db88dd19b79e00993e394b7ba7c328e9
Mikhail Tsimbalov <tmvrus83@gmail.com>
2024-10-03 13:18:43 +0300

    separate logger package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,46 +7,14 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/tmvrus/key-value-storage/internal/config"
+	"github.com/tmvrus/key-value-storage/internal/logger"
 	"github.com/tmvrus/key-value-storage/internal/server"
 	"github.com/tmvrus/key-value-storage/internal/storage"
 )
 
 const defaultConfigFile = "./config.yml"
-
-func initLogger(fileName, level string) *slog.Logger {
-	w := os.Stdout
-	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModeAppend|os.ModeExclusive)
-	if err != nil {
-		slog.Error("failed to open file for logging, use stdout", err.Error(), "file_name", fileName)
-	} else {
-		go func() {
-			t := time.NewTicker(time.Second)
-			for range t.C {
-				if err := f.Sync(); err != nil {
-					slog.Error("failed to sync log file", "file_name", fileName, "error", err.Error())
-				}
-			}
-		}()
-
-		w = f
-	}
-
-	m := map[string]slog.Level{
-		"info":  slog.LevelInfo,
-		"debug": slog.LevelDebug,
-		"error": slog.LevelError,
-	}
-	l, ok := m[level]
-	if !ok {
-		slog.Error("failed to find log level, use debug", "level", level)
-		l = slog.LevelDebug
-	}
-
-	return slog.New(slog.NewJSONHandler(w, &slog.HandlerOptions{Level: l}))
-}
 
 func main() {
 	cxt, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM)
@@ -62,7 +30,7 @@ func main() {
 		slog.Error("failed to fill config, use default values", "error", err.Error())
 	}
 
-	log := initLogger(cfg.Logging.Output, cfg.Logging.Level)
+	log := logger.New(cfg.Logging.Output, cfg.Logging.Level)
 
 	err = server.
 		New(cfg, storage.New(cfg), log).
---
+++ internal/logger/logger.go
@@ -0,0 +1,39 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+	"time"
+)
+
+func New(fileName, level string) *slog.Logger {
+	w := os.Stdout
+	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModeAppend|os.ModeExclusive)
+	if err != nil {
+		slog.Error("failed to open file for logging, use stdout", err.Error(), "file_name", fileName)
+	} else {
+		go func() {
+			t := time.NewTicker(time.Second)
+			for range t.C {
+				if err := f.Sync(); err != nil {
+					slog.Error("failed to sync log file", "file_name", fileName, "error", err.Error())
+				}
+			}
+		}()
+
+		w = f
+	}
+
+	m := map[string]slog.Level{
+		"info":  slog.LevelInfo,
+		"debug": slog.LevelDebug,
+		"error": slog.LevelError,
+	}
+	l, ok := m[level]
+	if !ok {
+		slog.Error("failed to find log level, use debug", "level", level)
+		l = slog.LevelDebug
+	}
+
+	return slog.New(slog.NewJSONHandler(w, &slog.HandlerOptions{Level: l}))
+}
